f709a0e45146ab4392bb117813ef992d2066dab8
chrisekwugum <chrisekwugum@gmail.com>
2023-12-14 21:24:46 +0100

    This is a midified version, this code can count words

--- code_work2.py
+++ code_work2.py
@@ -2,8 +2,9 @@ def count_letters():
     word = input("Please enter a sentence: ").lower()
     count = len(word)
     count2 = len(word.replace(" ",""))
+    count3 = len(word.split())
 
-    print(f'Your sentence is {count2} characters without spaces and {count} with spaces')
+    print(f'Your sentence has a total of {count3 } words, {count2} characters without spaces and {count} with spaces.')
     
 
 count_letters()
